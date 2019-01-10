a086531c20bb3c4d600b93635994a193794ca147
Justin Cranshaw <justin@cranshaw.me>
2019-01-09 22:37:23 -0800

    updated button and text

--- src/App.js
+++ src/App.js
@@ -22,9 +22,17 @@ class App extends Component {
         <h2>
           March 2nd, Populuxe Brewing, 2 to 7pm.
         </h2>  
-        <a href="https://raincitymarket.typeform.com/to/u8Jwdi" className="button">Send us your shop ideas</a> 
-        <div className="contact">
-          Have a popup shop? Contact <a href="mailto:hi@opencity.market">hi@opencity.market</a>.
+        <a href="https://raincitymarket.typeform.com/to/u8Jwdi" className="button">
+          Cast Your Vote
+        </a> 
+   
+        <div className="contact-container">
+          <div className="contact-label">
+            Selling at the Market
+          </div>
+          <div className="contact">
+            Have a popup shop? Contact <a href="mailto:hi@opencity.market">hi@opencity.market</a>.
+          </div>
         </div>
       </div>
     );
