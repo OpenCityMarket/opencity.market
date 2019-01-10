6f3c029319514bf89e1c01508eaeea425306997f
Justin Cranshaw <justin@cranshaw.me>
2019-01-10 08:41:44 -0800

    missing ;

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import App from './App';
-import Thanks from './Thanks'
-import { Route, Switch, Redirect } from 'react-router-dom';
+import Thanks from './Thanks';
+import { Route, Switch } from 'react-router-dom';
 
 export const Routes = () => {
     return (
         <div>
             <Switch>
                 <Route exact path="/" component={App} />
-                <Route exact path="/thanks" component={Thanks} />
+                <Route path="/thanks" component={Thanks} />
             </Switch>
         </div>
     );
