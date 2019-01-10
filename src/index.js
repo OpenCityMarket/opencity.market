32e57ff3c3a94a8e0b35fe9979bc52609b007251
Justin Cranshaw <justin@cranshaw.me>
2019-01-10 08:28:59 -0800

    updated router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './Routes';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Router>
@@ -12,4 +11,3 @@ ReactDOM.render(
     document.getElementById('root'),
 );
 
-serviceWorker.unregister();
