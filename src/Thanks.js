97db717cfd0e04c62c0e733aa5e2eb99c56a759b
Justin Cranshaw <justin@cranshaw.me>
2019-01-17 12:18:06 -0800

    moved page embed back to javascript

--- src/Thanks.js
+++ src/Thanks.js
@@ -13,7 +13,7 @@ class Thanks extends Component {
         <h1>
           To get updates,<br />follow our Facebook Event!
         </h1>
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fopencitymkt%2F%3Feid%3DARAXQjOSlzupVqxusYsVVndgjyFSfshRmzpUYpWdueNnTVAHLCxaGs6TNlaWZCc1T7--xr_twt4uoRF2&tabs=events&width=340&height=350&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=231965911026130" width={340} height={350} style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameBorder={0} allowtransparency="true" allow="encrypted-media" />        
+        <div className="fb-page" data-href="https://www.facebook.com/opencitymkt/" data-tabs="events" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/opencitymkt/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/opencitymkt/">Open City Market</a></blockquote></div>
         <div id="share">
           <a className="social" href="https://www.facebook.com/opencitymkt/" target="blank"><FontAwesomeIcon icon={faFacebookF} /></a>
           <a className="social" href="https://twitter.com/opencitymarket" target="blank"><FontAwesomeIcon icon={faTwitter} /></a>
