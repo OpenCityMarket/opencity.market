41bbff2a994b8f23f2239a45e4233a1ce5a16b3d
Justin Cranshaw <justin@cranshaw.me>
2019-01-17 00:10:44 -0800

    updated fb event

--- src/Thanks.js
+++ src/Thanks.js
@@ -11,10 +11,10 @@ class Thanks extends Component {
       <div className="Thanks">
         <img src={logoH} className="logo" alt="logo" />
         <h1>
-          Stay Up To Date. Follow our Facebook Event!
+          Stay Up To Date.<br />Follow our Facebook Event!
         </h1>
 
-        <div class="fb-page" data-href="https://www.facebook.com/opencitymkt/" data-tabs="events" data-width="400" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/opencitymkt/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/opencitymkt/">Open City Market</a></blockquote></div>
+        <div class="fb-page" data-href="https://www.facebook.com/opencitymkt/" data-tabs="events" data-width="400" data-height="550" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/opencitymkt/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/opencitymkt/">Open City Market</a></blockquote></div>
 
         <div id="share">
           <a className="social" href="https://www.facebook.com/opencitymkt/" target="blank"><FontAwesomeIcon icon={faFacebookF} /></a>
