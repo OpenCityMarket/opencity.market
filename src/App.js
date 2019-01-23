af58de593e0c9ddc5782e9b8e24ff38026238ef4
Justin Cranshaw <justin@cranshaw.me>
2019-01-23 08:09:37 -0800

    updated button

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>A new popup market in Seattle,<br/>where <span className="em">you</span> decide what shops to feature</h1>
         <h2>March 2nd, Populuxe Brewing, 12 to 5pm</h2>  
-        <a href={this.typeformUrl} className="button">Suggest Popups for the Market</a> 
+        <a href={this.typeformUrl} className="button">Suggest Popups!</a> 
         <div className="contact-container">
           <div className="contact-label">Have a popup shop?</div>
           <div className="contact">Contact <a href="mailto:hi@opencity.market">hi@opencity.market</a></div>
