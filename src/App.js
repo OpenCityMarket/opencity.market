f08bbb02359e14762cd3508f628116c8e8c97209
Justin Cranshaw <justin@cranshaw.me>
2019-01-30 14:29:03 -0800

    updated button CTA

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super(props);
     const values = queryString.parse(this.props.location.search);
     this.typeformUrl = "https://opencitymarket.typeform.com/to/ONat9h";
+    this.eventUrl = "https://www.facebook.com/events/226887871572780/";
     if (values.src) {
       this.typeformUrl = this.typeformUrl + "?src=" + values.src;
     } else {
@@ -21,7 +22,7 @@ class App extends Component {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>A new popup market in Seattle,<br/>where <span className="em">you</span> decide what shops to feature</h1>
         <h2>March 2nd, Populuxe Brewing, 12 to 5pm</h2>  
-        <a href={this.typeformUrl} className="button">Suggest Popups!</a> 
+        <a href={this.eventUrl} className="button">Get on the Guest List!</a> 
         <div className="contact-container">
           <div className="contact-label">Have a popup shop?</div>
           <div className="contact">Contact <a href="mailto:hi@opencity.market">hi@opencity.market</a></div>
