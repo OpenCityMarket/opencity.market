81c362158ceebabe7949f620dd4e8532fe76fb0d
Justin Cranshaw <justin@cranshaw.me>
2019-02-11 08:08:17 -0800

    componentized vendor grid

--- src/App.js
+++ src/App.js
@@ -1,84 +1,11 @@
 import React, { Component } from 'react';
-import StackGrid, { transitions, easings } from "react-stack-grid";
-import sizeMe from 'react-sizeme';
-import { Card } from 'antd';
 import queryString from 'query-string';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import VendorGrid from './VendorGrid';
 import logo from './images/ocm-logo-horizontal.png';
 
-import v1_1 from './images/vendors/v1_1.jpg';
-import v1_2 from './images/vendors/v1_2.jpg';
-import v1_3 from './images/vendors/v1_3.jpg';
-import v1_4 from './images/vendors/v1_4.jpg';
-import v1_5 from './images/vendors/v1_5.jpg';
-import v2_1 from './images/vendors/v2_1.jpg';
-import v2_2 from './images/vendors/v2_2.jpg';
-import v2_3 from './images/vendors/v2_3.jpg';
-import v2_4 from './images/vendors/v2_4.jpg';
-import v2_5 from './images/vendors/v2_5.jpg';
-import v2_6 from './images/vendors/v2_6.jpg';
-import v2_7 from './images/vendors/v2_7.jpg';
-import v2_8 from './images/vendors/v2_8.jpg';
-import v3_1 from './images/vendors/v3_1.jpg';
-import v3_2 from './images/vendors/v3_2.jpg';
-import v3_3 from './images/vendors/v3_3.jpg';
-import v3_4 from './images/vendors/v3_4.jpg';
-import v3_5 from './images/vendors/v3_5.jpg';
-import v4_1 from './images/vendors/v4_1.jpg';
-import v4_2 from './images/vendors/v4_2.jpg';
-import v4_3 from './images/vendors/v4_3.jpg';
-import v4_4 from './images/vendors/v4_4.jpg';
-import v4_5 from './images/vendors/v4_5.jpg';
-import v4_6 from './images/vendors/v4_6.jpg';
-import v4_7 from './images/vendors/v4_7.jpg';
-import v5_1 from './images/vendors/v5_1.jpg';
-import v5_2 from './images/vendors/v5_2.jpg';
-import v5_3 from './images/vendors/v5_3.jpg';
-import v5_4 from './images/vendors/v5_4.jpg';
-import h_1 from './images/vendors/h_1.jpg';
-import h_2 from './images/vendors/h_2.jpg';
-import h_3 from './images/vendors/h_3.jpg';
-import h_4 from './images/vendors/h_4.jpg';
-import h_5 from './images/vendors/h_5.jpg';
-import h_6 from './images/vendors/h_6.jpg';
-import v6_1 from './images/vendors/v6_1.jpg';
-
 import './App.css';
-
-const transition = transitions.scaleDown;
-
-const { Meta } = Card;
-
-const renderCard = ({key, vendor, description, url, img}) => (
-  <a href={url} className="card" target="_blank" rel="noopener noreferrer">
-    <Card key={key} style={{}} cover={ <img alt="" src={img} /> }>
-      <Meta
-      title={vendor}
-      description={description}
-      />
-    </Card>
-  </a>
-)
-
-const shuffle = function(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-
-let images = [
-  { key: 'h_5', img: h_5, vendor: 'Populuxe Brewing', description: 'Our host and friendly neighborhood brewery; doggos and kiddos welcome', url: 'https://www.populuxebrewing.com/' },
-  { key: 'v1_3', img: v1_3, vendor: 'Raised Doughnuts', description: 'Elevated fried confections from the Central District', url: 'https://www.raiseddoughnuts.com/' },
-  { key: 'v2_2', img: v2_2, vendor: 'MODHome Ceramics', description: 'Rustic modern clay works', url: 'https://www.modhomeceramics.com/' },
-  { key: 'v3_1', img: v3_1, vendor: 'Magic Magpie Studio', description: 'Henna Art by Antoinette', url: 'http://antoin.net/' },
-  { key: 'v4_2', img: v4_2, vendor: 'Frelard Tamales', description: 'Authentic and handmade tamales, with vegan-friendly options', url: 'http://www.frelardtamales.com/' },
-  { key: 'v5_4', img: v5_4, vendor: 'Peasant Food Manifesto', description: 'Korean-inspired street food fusions', url: 'http://www.peasantfoodmanifesto.com/' },
-  { key: 'v6_1', img: v6_1, vendor: 'Blue Highway Games', description: 'Cards and board games available for you to play and buy', url: 'http://www.bluehighwaygames.com' },
-];
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -94,7 +21,6 @@ class App extends Component {
    }
 
   render() {
-    const { width, height } = this.props.size
     return (
       <div className="App">
 
@@ -118,22 +44,8 @@ class App extends Component {
             based on availability and space constraints.
           </p>
 
-          <StackGrid
-            monitorImagesLoaded
-            columnWidth={ 255 }          
-            duration={600}
-            gutterWidth={24}
-            gutterHeight={24}
-            easing={easings.cubicOut}
-            appearDelay={60}
-            appear={transition.appear}
-            appeared={transition.appeared}
-            enter={transition.enter}
-            entered={transition.entered}
-            leaved={transition.leaved}
-          >
-            { images.map(renderCard) }
-          </StackGrid>
+          <VendorGrid />
+
         </div>
 
         <div className="section section-three">
@@ -153,4 +65,4 @@ class App extends Component {
   }
 }
 
-export default sizeMe()(App);
+export default App;
---
+++ src/VendorGrid.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import StackGrid, { transitions, easings } from "react-stack-grid";
+import sizeMe from 'react-sizeme';
+import { Card } from 'antd';
+
+import v1_3 from './images/vendors/v1_3.jpg';
+import v2_2 from './images/vendors/v2_2.jpg';
+import v3_1 from './images/vendors/v3_1.jpg';
+import v4_2 from './images/vendors/v4_2.jpg';
+import v5_4 from './images/vendors/v5_4.jpg';
+import h_5 from './images/vendors/h_5.jpg';
+import v6_1 from './images/vendors/v6_1.jpg';
+
+import './VendorGrid.css';
+
+const transition = transitions.scaleDown;
+
+const { Meta } = Card;
+
+const renderCard = ({key, vendor, description, url, img}) => (
+  <a href={url} className="card" target="_blank" rel="noopener noreferrer">
+    <Card key={key} style={{}} cover={ <img alt="" src={img} /> }>
+      <Meta
+      title={vendor}
+      description={description}
+      />
+    </Card>
+  </a>
+)
+
+let images = [
+  { key: 'h_5', img: h_5, vendor: 'Populuxe Brewing', description: 'Our host and friendly neighborhood brewery; doggos and kiddos welcome', url: 'https://www.populuxebrewing.com/' },
+  { key: 'v1_3', img: v1_3, vendor: 'Raised Doughnuts', description: 'Elevated fried confections from the Central District', url: 'https://www.raiseddoughnuts.com/' },
+  { key: 'v2_2', img: v2_2, vendor: 'MODHome Ceramics', description: 'Rustic modern clay works', url: 'https://www.modhomeceramics.com/' },
+  { key: 'v3_1', img: v3_1, vendor: 'Magic Magpie Studio', description: 'Henna Art by Antoinette', url: 'http://antoin.net/' },
+  { key: 'v4_2', img: v4_2, vendor: 'Frelard Tamales', description: 'Authentic and handmade tamales, with vegan-friendly options', url: 'http://www.frelardtamales.com/' },
+  { key: 'v5_4', img: v5_4, vendor: 'Peasant Food Manifesto', description: 'Korean-inspired street food fusions', url: 'http://www.peasantfoodmanifesto.com/' },
+  { key: 'v6_1', img: v6_1, vendor: 'Blue Highway Games', description: 'Cards and board games available for you to play and buy', url: 'http://www.bluehighwaygames.com' },
+];
+
+class VendorGrid extends Component {
+
+  render() {
+    const { width, height } = this.props.size
+    return (
+        <StackGrid
+            monitorImagesLoaded
+            columnWidth={ 255 }          
+            duration={600}
+            gutterWidth={24}
+            gutterHeight={24}
+            easing={easings.cubicOut}
+            appearDelay={60}
+            appear={transition.appear}
+            appeared={transition.appeared}
+            enter={transition.enter}
+            entered={transition.entered}
+            leaved={transition.leaved}
+        >
+            { images.map(renderCard) }
+        </StackGrid>
+    );
+  }
+}
+
+export default sizeMe()(VendorGrid);
